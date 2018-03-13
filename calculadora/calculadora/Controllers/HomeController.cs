6f649be40b558f02310ced6090623b726f5148ed
DESKTOP-12VEQ01\Jorge Mendes <mendesjorge49@gmail,com>
2018-03-13 10:14:50 +0000

    cont

--- calculadora/calculadora/Controllers/HomeController.cs
+++ calculadora/calculadora/Controllers/HomeController.cs
@@ -15,8 +15,28 @@ namespace calculadora.Controllers
         }
         // POST: Home
         [HttpPost]
-        public ActionResult Index(string bt,string name)
+        public ActionResult Index(string bt)
         {
+            double resultado = 0;
+            switch (bt)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    ViewBag.bt = bt;
+                    break;
+                default:
+                    bt ="0";
+                    ViewBag.bt = bt;
+                    break;
+            }
             return View();
         }
     }
